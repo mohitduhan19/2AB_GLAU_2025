bf23b4deb4642921a54624025b199543eca877ab
Mohit Duhan <mohitkumar72956@gmail.com>
2025-03-24 15:38:50 +0530

    Linked List

---
+++ Lec3/Middle_Linked_List.java
@@ -0,0 +1,20 @@
+package AB.Lec3;
+
+public class Middle_Linked_List {
+     class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode middleNode(ListNode head) {
+        ListNode f = head.next , s = head;
+        while(f != null && f.next != null){
+            s = s.next;
+            f = f.next.next;
+        }
+        if(f != null )return s.next;
+        return s;
+    }
+}
